978b5588ae05da2e8f79a6426542ce0321ab8a90
RKPatel2004 <rudra0405@gmail.com>
2025-07-05 10:48:01 +0530

    multiple numbers string test cases

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,10 @@
 function add(numbers)
 {
-    return 0;
+    if(numbers === "") return 0;
+
+    const nums = numbers.split(",");
+
+    return nums.reduce((acc, num) => acc + parseInt(num), 0);
 }
 
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,15 @@ const add = require("./stringCalculator");
 test('return 0 for empty input string', () =>{
     expect(add("")).toBe(0);
 });
+
+test('return number in case of single length string', () =>{
+    expect(add("1")).toBe(1);
+});
+
+test('adding two numbers', () =>{
+    expect(add("1, 2")).toBe(3);
+});
+
+test('adding three numbers', () =>{
+    expect(add("16, 4, 31")).toBe(51);
+});
